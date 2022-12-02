f188107d50902b974ae157c96e05bd2949937d7e
Maksim Buryshynets <Maksim_Buryshynets@epam.com>
2022-12-02 12:00:32 +0500

    day 02: refactoring - little less performance, more readability;

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,33 +1,33 @@
 fun main() {
     fun part1(input: Sequence<String>) = solvePuzzle(
         input, resultTable = mapOf(
-            "AX" to 3 + 1,
-            "AY" to 6 + 2,
-            "AZ" to 0 + 3,
+            "AX" to Shape.ROCK     + Round.DRAW,
+            "AY" to Shape.PAPER    + Round.WIN,
+            "AZ" to Shape.SCISSORS + Round.LOOSE,
 
-            "BX" to 0 + 1,
-            "BY" to 3 + 2,
-            "BZ" to 6 + 3,
+            "BX" to Shape.ROCK     + Round.LOOSE,
+            "BY" to Shape.PAPER    + Round.DRAW,
+            "BZ" to Shape.SCISSORS + Round.WIN,
 
-            "CX" to 6 + 1,
-            "CY" to 0 + 2,
-            "CZ" to 3 + 3,
+            "CX" to Shape.ROCK     + Round.WIN,
+            "CY" to Shape.PAPER    + Round.LOOSE,
+            "CZ" to Shape.SCISSORS + Round.DRAW,
         )
     )
 
     fun part2(input: Sequence<String>) = solvePuzzle(
         input, resultTable = mapOf(
-            "AX" to 0 + 3,
-            "AY" to 3 + 1,
-            "AZ" to 6 + 2,
+            "AX" to Round.LOOSE + Shape.SCISSORS,
+            "AY" to Round.DRAW  + Shape.ROCK,
+            "AZ" to Round.WIN   + Shape.PAPER,
 
-            "BX" to 0 + 1,
-            "BY" to 3 + 2,
-            "BZ" to 6 + 3,
+            "BX" to Round.LOOSE + Shape.ROCK,
+            "BY" to Round.DRAW  + Shape.PAPER,
+            "BZ" to Round.WIN   + Shape.SCISSORS,
 
-            "CX" to 0 + 2,
-            "CY" to 3 + 3,
-            "CZ" to 6 + 1,
+            "CX" to Round.LOOSE + Shape.PAPER,
+            "CY" to Round.DRAW  + Shape.SCISSORS,
+            "CZ" to Round.WIN   + Shape.ROCK,
         )
     )
 
@@ -38,3 +38,11 @@ fun main() {
 private fun solvePuzzle(input: Sequence<String>, resultTable: Map<String, Int>): Int {
     return input.map { it.replace(" ", "") }.mapNotNull(resultTable::get).sum()
 }
+
+private interface Scored {
+    val score: Int
+    operator fun plus(that: Scored) = this.score + that.score
+}
+
+private enum class Round(override val score: Int) : Scored { LOOSE(0), DRAW(3), WIN(6) }
+private enum class Shape(override val score: Int) : Scored { ROCK(1), PAPER(2), SCISSORS(3) }
