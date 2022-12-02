3a03d90f38979700fbb80261a7386c11a4484014
Maksim Buryshynets <Maksim_Buryshynets@epam.com>
2022-12-02 10:16:24 +0500

    day 01: naming changes;

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,12 +1,12 @@
 fun main() {
     fun part1(input: Sequence<String>) = solvePuzzle(input)
-    fun part2(input: Sequence<String>) = solvePuzzle(input, 3)
+    fun part2(input: Sequence<String>) = solvePuzzle(input, numberOfItemsToTake = 3)
 
     println(readInput("Day01", ::part1))
     println(readInput("Day01", ::part2))
 }
 
-private fun solvePuzzle(input: Sequence<String>, take: Int = 1): Int =
+private fun solvePuzzle(input: Sequence<String>, numberOfItemsToTake: Int = 1): Int =
     input.fold(initial = mutableListOf(0)) { list, line ->
         list.apply {
             if (line.isEmpty()) {
@@ -15,4 +15,4 @@ private fun solvePuzzle(input: Sequence<String>, take: Int = 1): Int =
                 list[lastIndex] = list.last() + line.toInt()
             }
         }
-    }.sortedDescending().take(take).sum()
+    }.sortedDescending().take(numberOfItemsToTake).sum()
