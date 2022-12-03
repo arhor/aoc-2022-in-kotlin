0675230e7c5acf0592bcf3069aca333bd0021053
Maksim Buryshynets <Maksim_Buryshynets@epam.com>
2022-12-03 10:32:00 +0500

    day 03: complete;

---
+++ src/Day03.kt
@@ -0,0 +1,22 @@
+fun main() {
+    val input = readInput("Day03")
+
+    println("Part 1: " + solvePuzzle1(input))
+    println("Part 2: " + solvePuzzle2(input))
+}
+
+private const val ALPHABET = " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+private fun solvePuzzle1(input: List<String>): Int {
+    return input.sumOf { line ->
+        val l = line.substring(0, line.length / 2)
+        val r = line.substring(line.length / 2, line.length)
+        ALPHABET.indexOf(l.first { it in r })
+    }
+}
+
+private fun solvePuzzle2(input: List<String>): Int {
+    return input.chunked(3).sumOf { (s1, s2, s3) ->
+        ALPHABET.indexOf(s1.first { it in s2 && it in s3 })
+    }
+}
