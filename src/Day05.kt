ddeef7f7021fa5f26d958ada701c0f34f60a0caa
Maksim Buryshynets <Maksim_Buryshynets@epam.com>
2022-12-06 00:19:05 +0500

    day 05: refactored;

--- src/Day05.kt
+++ src/Day05.kt
@@ -3,48 +3,35 @@ import java.util.*
 fun main() {
     val input = readInput {}
 
-    println("Part 1: " + solvePuzzle(input))
-    println("Part 2: " + solvePuzzle2(input))
+    println("Part 1: " + solvePuzzle(input, insertReversed = true))
+    println("Part 2: " + solvePuzzle(input, insertReversed = false))
 }
 
-private val cratePattern = Regex("(\\[([A-Z])])|( {3})")
+private val cratePattern = Regex("(\\[([A-Z])])| {3}")
 private val instrPattern = Regex("^move ([0-9]+) from ([0-9]+) to ([0-9]+)$")
 
-private fun solvePuzzle(input: List<String>): String {
-    val table = TreeMap<Int, Deque<String>>()
-
-    for (line in input) {
-        cratePattern.findAll(line)
-            .map { it.groupValues[2] }
-            .forEachIndexed { i, crate ->
-                if (crate.isNotEmpty()) {
-                    table.computeIfAbsent(i + 1) { LinkedList() }.push(crate)
-                }
-            }
-        instrPattern.find(line)?.destructured?.toList()?.map { it.toInt() }?.let { (number, from, to) ->
-            repeat(number) {
-                table[to]!!.add(table[from]!!.removeLast())
-            }
-        }
-    }
-    return table.values.joinToString(separator = "") { it.last }
-}
-
-private fun solvePuzzle2(input: List<String>): String {
+private fun solvePuzzle(input: List<String>, insertReversed: Boolean): String {
     val table = TreeMap<Int, LinkedList<String>>()
 
     for (line in input) {
-        cratePattern.findAll(line)
-            .map { it.groupValues[2] }
-            .forEachIndexed { i, crate ->
-                if (crate.isNotEmpty()) {
-                    table.computeIfAbsent(i + 1) { LinkedList() }.push(crate)
-                }
+        cratePattern.findAll(line).map { it.groupValues[2] }.forEachIndexed { i, crate ->
+            if (crate.isNotEmpty()) {
+                table.computeIfAbsent(i + 1) { LinkedList() }.push(crate)
             }
+        }
         instrPattern.find(line)?.destructured?.toList()?.map { it.toInt() }?.let { (number, from, to) ->
-            val insertPos = table[to]!!.lastIndex
-            repeat(number) {
-                table[to]!!.add(insertPos + 1, table[from]!!.removeLast())
+            val source = table[from]!!
+            val target = table[to]!!
+
+            if (insertReversed) {
+                repeat(number) {
+                    target.add(source.removeLast())
+                }
+            } else {
+                val insertPos = target.lastIndex
+                repeat(number) {
+                    target.add(insertPos + 1, source.removeLast())
+                }
             }
         }
     }
