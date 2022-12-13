2ae043d0bd5f78b78c1ffcff21e1698b80af772e
Maksim Buryshynets <Maksim_Buryshynets@epam.com>
2022-12-13 20:33:02 +0300

    day 13: part 2 complete;

--- src/Day12.kt
+++ src/Day12.kt
@@ -3,8 +3,8 @@ import java.util.*
 fun main() {
     val input = readInput {}
 
-//    println("Part 1: ${solvePuzzle(input)}")
-    println("Part 2: ${solvePuzzle2(input)}")
+    println("Part 1: ${solvePuzzle(input)}")
+//    println("Part 2: ${solvePuzzle2(input)}")
 }
 
 private fun solvePuzzle(list: List<String>): Double {
@@ -16,41 +16,41 @@ private fun solvePuzzle(list: List<String>): Double {
     return dijkstra(start, end, squares)
 }
 
-private fun solvePuzzle2(list: List<String>): Double {
-    val squares = getSquares2(list)
+//private fun solvePuzzle2(list: List<String>): Double {
+//    val squares = getSquares2(list)
+//
+//    val starts = squares.filter { it.isStart }
+//    val end = squares.first { it.isEnd }
+//
+//    println("Found ${starts.size} possible start points")
+//
+//    return starts.withIndex().toList().parallelStream()
+//        .mapToDouble {
+//            println("Processing ${it.index + 1} point")
+//            dijkstra(it.value, end, squares)
+//        }
+//        .min()
+//        .asDouble
+//}
 
-    val starts = squares.filter { it.isStart }
-    val end = squares.first { it.isEnd }
-
-    println("Found ${starts.size} possible start points")
-
-    return starts.withIndex().toList().parallelStream()
-        .mapToDouble {
-            println("Processing ${it.index + 1} point")
-            dijkstra(it.value, end, squares)
-        }
-        .min()
-        .asDouble
-}
-
-private fun dijkstra(start: Square, end: Square, squares: List<Square>): Double {
-    val queue = PriorityQueue<Square>().apply { offer(start) }
-    val distances = HashMap<Square, Double>().apply { put(start, 0.0) }
+private fun dijkstra(alpha: Square, omega: Square, squares: List<Square>): Double {
+    val queue = PriorityQueue<Square>().apply { offer(alpha) }
+    val distances = HashMap<Square, Double>().apply { put(alpha, 0.0) }.withDefault { Double.POSITIVE_INFINITY }
 
     while (!queue.isEmpty()) {
         val currPoint = queue.poll()
-        val oldDistance = distances[currPoint]!!
-        val newDistance =
-            oldDistance + 1 // increase by 1 since distance to the closest points is always 1 in this exact case
+        val oldDistance = distances.getValue(currPoint)
+        // increase by 1 since distance to the closest points is always 1 in this exact case
+        val newDistance = oldDistance + 1
 
         for (nextPoint in currPoint.getNeighbours(squares)) {
-            if (newDistance < (distances[nextPoint] ?: Double.POSITIVE_INFINITY)) {
+            if (newDistance < distances.getValue(nextPoint)) {
                 distances[nextPoint] = newDistance
                 queue.add(nextPoint)
             }
         }
     }
-    return distances.filterKeys { it == end }.values.minOrNull() ?: Double.POSITIVE_INFINITY
+    return distances.filterKeys { it == omega }.values.minOrNull() ?: Double.POSITIVE_INFINITY
 }
 
 private fun getSquares(input: List<String>): List<Square> = buildList {
@@ -75,33 +75,33 @@ private fun getSquares(input: List<String>): List<Square> = buildList {
     }
 }
 
-private fun getSquares2(input: List<String>): List<Square> = buildList {
-    for ((y, line) in input.withIndex()) {
-        for ((x, height) in line.withIndex()) {
-            add(
-                Square(x, y, height.code).also {
-                    when (height) {
-                        'a' -> {
-                            if (x == 0 || x == line.lastIndex || y == 0 || y == input.lastIndex) {
-                                it.isStart = true
-                            }
-                        }
-
-                        'S' -> {
-                            it.height = 'a'.code
-                            it.isStart = true
-                        }
-
-                        'E' -> {
-                            it.height = 'z'.code
-                            it.isEnd = true
-                        }
-                    }
-                }
-            )
-        }
-    }
-}
+//private fun getSquares2(input: List<String>): List<Square> = buildList {
+//    for ((y, line) in input.withIndex()) {
+//        for ((x, height) in line.withIndex()) {
+//            add(
+//                Square(x, y, height.code).also {
+//                    when (height) {
+//                        'a' -> {
+//                            if (x == 0 || x == line.lastIndex || y == 0 || y == input.lastIndex) {
+//                                it.isStart = true
+//                            }
+//                        }
+//
+//                        'S' -> {
+//                            it.height = 'a'.code
+//                            it.isStart = true
+//                        }
+//
+//                        'E' -> {
+//                            it.height = 'z'.code
+//                            it.isEnd = true
+//                        }
+//                    }
+//                }
+//            )
+//        }
+//    }
+//}
 
 data class Square(val x: Int, val y: Int) : Comparable<Square> {
 
