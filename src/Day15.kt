815d2c984eb1feec39b1c782491bdfe2743a1f42
Maksim Buryshynets <Maksim_Buryshynets@epam.com>
2022-12-15 15:56:11 +0300

    day 15: part 1 complete;

--- src/Day15.kt
+++ src/Day15.kt
@@ -1,4 +1,4 @@
-import java.util.*
+import kotlin.math.abs
 
 fun main() {
     val input = readInput {}
@@ -9,17 +9,39 @@ fun main() {
 private val inputPattern =
     Regex("^Sensor at x=(-?[0-9]+), y=(-?[0-9]+): closest beacon is at x=(-?[0-9]+), y=(-?[0-9]+)$")
 
-private enum class BeaconType { S, B }
-
-private fun solvePuzzle1(input: List<String>) {
-    val table = TreeMap<Point, BeaconType>()
+private fun solvePuzzle1(input: List<String>): Int {
+    val table = HashMap<Point, Char>()
 
     for (line in input) {
-        inputPattern.find(line)?.let {
-            val (x1, y1, x2, y2) = it.groupValues.drop(1).map(String::toInt)
+        inputPattern.find(line)?.let { matchResult ->
+            val (x1, y1, x2, y2) = matchResult.groupValues.drop(1).map(String::toInt)
 
-            table[Point(x1, y1)] = BeaconType.S
-            table[Point(x2, y2)] = BeaconType.B
+            val scaner = Point(x1, y1).also { table[it] = 'S' }
+            val beacon = Point(x2, y2).also { table[it] = 'B' }
+
+            for (point in determineTaxicabCircle(start = scaner, end = beacon).filter { it !in table.keys }) {
+                table[point] = '#'
+            }
+        }
+    }
+
+    return table.filterKeys { it.y == 2000000 }.filterValues { it != 'B' }.count()
+}
+
+private fun determineTaxicabCircle(start: Point, end: Point) = buildSet {
+    val length = maxOf(end.x, start.x) - minOf(end.x, start.x) + maxOf(end.y, start.y) - minOf(end.y, start.y)
+
+    val yMin = start.y - length
+    val yMax = start.y + length
+
+    for (y in (yMin..yMax).filter { it == 2000000 }) {
+        val width = length - abs(y - start.y)
+
+        val xMin = start.x - width
+        val xMax = start.x + width
+
+        for (x in xMin..xMax) {
+            add(element = Point(x, y))
         }
     }
 }
